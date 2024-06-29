0e7a7007fb544e7a26617ce9318f9d1912a2307b
Artulo-Skywalker <wdnmddd1@outlook.com>
2024-06-29 13:41:56 +0800

    Delete assets/scripts directory

--- assets/scripts/items.js
+++
@@ -1,4 +0,0 @@
-const lib = require("lib");
-const items = lib.getClass("HeavyIndustry.content.HIItems");
-global.items = items;
-module.exports = items;
--- assets/scripts/main.js
+++
@@ -1,5 +0,0 @@
-//Yes, as you can see, a significant portion of the Scripts content has been converted to Java reset.
-require("lib");
-require("items");
-require("blocks");
-require("tree");
