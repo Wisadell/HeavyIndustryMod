b48e581498016ea841e0bdb4a133b4fdf2206ff1
Wisadell <wdnmddd1@outlook.com>
2024-07-28 16:13:11 +0800

    Add files via upload

--- src/HeavyIndustry/HeavyIndustryMod.java
+++ src/HeavyIndustry/HeavyIndustryMod.java
@@ -5,11 +5,14 @@ import HeavyIndustry.gen.HIIcon;
 import HeavyIndustry.gen.HISounds;
 import HeavyIndustry.graphics.HICacheLayer;
 import HeavyIndustry.graphics.HIShaders;
+import HeavyIndustry.ui.dialogs.HIResearchDialog;
 import HeavyIndustry.world.meta.HIAttribute;
+import java.util.Objects;
 import arc.*;
 import arc.flabel.FLabel;
 import arc.scene.ui.Label;
 import arc.util.*;
+import mindustry.Vars;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
@@ -26,16 +29,16 @@ public class HeavyIndustryMod extends Mod{
         Log.info("Loaded HeavyIndustry Mod constructor.");
         Events.on(ClientLoadEvent.class, e -> {
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog(Core.bundle.format("mod.heavy-industry.name"));
+                BaseDialog dialog = new BaseDialog("@mod.heavy-industry.name");
                 dialog.buttons.button("@close", dialog::hide).size(100f, 50f);
                 dialog.cont.pane(table -> {
                     table.image(Core.atlas.find(name("img"))).left().row();
-                    table.add(Core.bundle.format("mod.heavy-industry.version")).left().growX().wrap().pad(4).labelAlign(Align.left).row();
-                    Label flabel1 = new FLabel(Core.bundle.format("mod.heavy-industry.author"));
+                    table.add("@mod.heavy-industry.version").left().growX().wrap().pad(4).labelAlign(Align.left).row();
+                    Label flabel1 = new FLabel("@mod.heavy-industry.author");
                     table.add(flabel1).left().row();
                     table.add("Heavy Industry Java Mod").left().growX().wrap().pad(4).labelAlign(Align.left).row();
-                    table.add(Core.bundle.format("mod.heavy-industry.note")).left().growX().wrap().width(550).maxWidth(600).pad(4).labelAlign(Align.left).row();
-                    table.add(Core.bundle.format("mod.heavy-industry.prompt")).left().growX().wrap().width(550).maxWidth(600).pad(4).labelAlign(Align.left).row();
+                    table.add("@mod.heavy-industry.note").left().growX().wrap().width(550).maxWidth(600).pad(4).labelAlign(Align.left).row();
+                    table.add("@mod.heavy-industry.prompt").left().growX().wrap().width(550).maxWidth(600).pad(4).labelAlign(Align.left).row();
                 }).grow().center().maxWidth(600);
                 dialog.show();
             });
@@ -69,5 +72,17 @@ public class HeavyIndustryMod extends Mod{
         HIPlanets.load();
         HISectorPresets.load();
         HITechTree.load();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        HIResearchDialog dialog = new HIResearchDialog();
+        ResearchDialog research = Vars.ui.research;
+        research.shown(() -> {
+            dialog.show();
+            Objects.requireNonNull(research);
+            Time.runTask(1f, research::hide);
+        });
     }
 }
