7cff6e15fb02ceeafe463daf74548fc3605c68bc
Wisadell <wdnmddd1@outlook.com>
2024-09-26 01:16:01 +0800

    Delete src/heavyindustry/gen directory

--- src/heavyindustry/gen/NoCoreDepositc.java
+++
@@ -1,9 +0,0 @@
-package heavyindustry.gen;
-
-import mindustry.gen.*;
-import mindustry.world.blocks.storage.*;
-
-@SuppressWarnings({"all", "unchecked", "deprecation"})
-public abstract interface NoCoreDepositc extends Boundedc, Builderc, Drawc, Entityc, Flyingc, Healthc, Hitboxc, Itemsc, Minerc, Physicsc, Posc, Rotc, Shieldc, Statusc, Syncc, Teamc, Unitc, Velc, Weaponsc {
-    CoreBlock.CoreBuild closestCore();
-}
