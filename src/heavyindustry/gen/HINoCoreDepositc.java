9cca42a2514662cdfb28532e907e0caf1382faa0
Wisadell <wdnmddd1@outlook.com>
2024-10-13 12:52:14 +0800

    Delete src/heavyindustry/gen directory

--- src/heavyindustry/gen/HINoCoreDepositc.java
+++
@@ -1,8 +0,0 @@
-package heavyindustry.gen;
-
-import mindustry.gen.*;
-import mindustry.world.blocks.storage.CoreBlock.*;
-
-public interface HINoCoreDepositc extends Boundedc, Builderc, Drawc, Entityc, Flyingc, Healthc, Hitboxc, Itemsc, Minerc, Physicsc, Posc, Rotc, Shieldc, Statusc, Syncc, Teamc, Unitc, Velc, Weaponsc {
-    CoreBuild closestCore();
-}
