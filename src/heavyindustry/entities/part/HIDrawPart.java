0c363065a751092997a6d9f4b095f23b7a567470
Wisadell <wdnmddd1@outlook.com>
2024-10-25 21:51:17 +0800

    Delete src/heavyindustry/entities/part directory

--- src/heavyindustry/entities/part/HIDrawPart.java
+++
@@ -1,9 +0,0 @@
-package heavyindustry.entities.part;
-
-import mindustry.entities.part.DrawPart.*;
-
-public class HIDrawPart {
-    public static final PartProgress
-            recoilWarmup = p -> Math.max(0, p.warmup - p.recoil),
-            recoilWarmupSep = p -> p.warmup - p.recoil;
-}
