845a07cdbdb98299dae824f278406cd3af0502b2
Wisadell <wdnmddd1@outlook.com>
2024-10-27 22:32:06 +0800

    Delete entities directory

--- entities/part/HIPartProgress.java
+++
@@ -1,9 +0,0 @@
-package heavyindustry.entities.part;
-
-import mindustry.entities.part.DrawPart.*;
-
-public class HIPartProgress {
-    public static final PartProgress
-            recoilWarmup = p -> Math.max(0, p.warmup - p.recoil),
-            recoilWarmupSep = p -> p.warmup - p.recoil;
-}
