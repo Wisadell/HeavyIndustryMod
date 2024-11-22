cd319708eaa33e6ed1e5d506880461e993729006
Wisadell <wdnmddd1@outlook.com>
2024-11-22 12:30:42 +0800

    Delete src/heavyindustry/world/meta/HIStatCat.java

--- src/heavyindustry/world/meta/HIStatCat.java
+++
@@ -1,9 +0,0 @@
-package heavyindustry.world.meta;
-
-import mindustry.world.meta.*;
-
-public final class HIStatCat {
-    public static final StatCat
-            structure = new StatCat("hi-structure"),
-            other = new StatCat("hi-other");
-}
